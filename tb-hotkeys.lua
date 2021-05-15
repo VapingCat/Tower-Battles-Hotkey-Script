d9a72bf21ef74d494167ce77aae9d9f5ea58a42a
Vapin' Cat <82176243+VapingCat@users.noreply.github.com>
2021-05-14 20:33:10 -0600

    Create tb-hotkeys.lua
    script

---
+++ tb-hotkeys.lua
@@ -0,0 +1,50 @@
+-- Game Link:
+-- https://www.roblox.com/games/45146873/Tower-Battles?refPageId=81b1e521-5de8-4344-b59e-4dcf6cdc365d
+
+-- Tower Defense Hotkeys
+-- Made By Vapin' Cat#5497
+
+-- not very hard to make at all..
+
+warn('Tower Defense Hotkeys')
+warn('Made By Vapin\' Cat#5497')
+
+if getgenv()['Tower Defense Hotkeys'] then
+    getgenv()['Tower Defense Hotkeys'].reset()
+end
+
+local connections = {}
+
+function bind(btn, key)
+    table.insert(connections,game:GetService("UserInputService").InputBegan:connect(function(inputObject, gameProcessedEvent)
+        if not gameProcessedEvent then
+            if inputObject.KeyCode == key then
+                for _,v in pairs(getconnections(btn.MouseButton1Down)) do
+                    v.Function()
+                end
+                for _,v in pairs(getconnections(btn.MouseButton1Click)) do
+                    v.Function()
+                end
+                for _,v in pairs(getconnections(btn.MouseButton1Up)) do
+                    v.Function()
+                end
+            end
+        end
+    end))
+end
+
+bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.Towers.Tower1.image, Enum.KeyCode.One)
+bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.Towers.Tower2.image, Enum.KeyCode.Two)
+bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.Towers.Tower3.image, Enum.KeyCode.Three)
+bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.Towers.Tower4.image, Enum.KeyCode.Four)
+bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.Towers.Tower5.image, Enum.KeyCode.Five)
+bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.TowerStats.Sell, Enum.KeyCode.Backspace)
+bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.TowerStats.Path1.Purchase, Enum.KeyCode.E)
+
+getgenv()['Tower Defense Hotkeys'] = {
+    ['reset'] = function()
+        for _,v in pairs(connections) do
+            v:Disconnect()
+        end
+    end
+}
