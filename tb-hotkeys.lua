4cb2677d84a4d15d711848de9ee51eb9158f7864
Vapin' Cat <82176243+VapingCat@users.noreply.github.com>
2021-05-14 20:38:25 -0600

    Update tb-hotkeys.lua

--- tb-hotkeys.lua
+++ tb-hotkeys.lua
@@ -9,42 +9,47 @@
 warn('Tower Defense Hotkeys')
 warn('Made By Vapin\' Cat#5497')
 
-if getgenv()['Tower Defense Hotkeys'] then
-    getgenv()['Tower Defense Hotkeys'].reset()
-end
-
 local connections = {}
 
-function bind(btn, key)
-    table.insert(connections,game:GetService("UserInputService").InputBegan:connect(function(inputObject, gameProcessedEvent)
-        if not gameProcessedEvent then
-            if inputObject.KeyCode == key then
-                for _,v in pairs(getconnections(btn.MouseButton1Down)) do
-                    v.Function()
-                end
-                for _,v in pairs(getconnections(btn.MouseButton1Click)) do
-                    v.Function()
-                end
-                for _,v in pairs(getconnections(btn.MouseButton1Up)) do
-                    v.Function()
-                end
-            end
-        end
-    end))
+function execute()
+	if getgenv()['Tower Defense Hotkeys'] then
+		getgenv()['Tower Defense Hotkeys'].reset()
+	end
+
+	function bind(btn, key)
+		table.insert(connections,game:GetService("UserInputService").InputBegan:connect(function(inputObject, gameProcessedEvent)
+			if not gameProcessedEvent then
+				if inputObject.KeyCode == key then
+					for _,v in pairs(getconnections(btn.MouseButton1Down)) do
+						v.Function()
+					end
+					for _,v in pairs(getconnections(btn.MouseButton1Click)) do
+						v.Function()
+					end
+					for _,v in pairs(getconnections(btn.MouseButton1Up)) do
+						v.Function()
+					end
+				end
+			end
+		end))
+	end
+
+	bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.Towers.Tower1.image, Enum.KeyCode.One)
+	bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.Towers.Tower2.image, Enum.KeyCode.Two)
+	bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.Towers.Tower3.image, Enum.KeyCode.Three)
+	bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.Towers.Tower4.image, Enum.KeyCode.Four)
+	bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.Towers.Tower5.image, Enum.KeyCode.Five)
+	bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.TowerStats.Sell, Enum.KeyCode.Backspace)
+	bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.TowerStats.Path1.Purchase, Enum.KeyCode.E)
 end
 
-bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.Towers.Tower1.image, Enum.KeyCode.One)
-bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.Towers.Tower2.image, Enum.KeyCode.Two)
-bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.Towers.Tower3.image, Enum.KeyCode.Three)
-bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.Towers.Tower4.image, Enum.KeyCode.Four)
-bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.Towers.Tower5.image, Enum.KeyCode.Five)
-bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.TowerStats.Sell, Enum.KeyCode.Backspace)
-bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.TowerStats.Path1.Purchase, Enum.KeyCode.E)
+game.Players.LocalPlayer.CharacterAdded:Connect(execute)
 
 getgenv()['Tower Defense Hotkeys'] = {
     ['reset'] = function()
         for _,v in pairs(connections) do
             v:Disconnect()
+			connections = {}
         end
     end
 }
