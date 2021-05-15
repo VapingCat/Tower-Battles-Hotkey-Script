7ddf8dd831cec1ef197cc53ea572a1ab21e2c651
Vapin' Cat <82176243+VapingCat@users.noreply.github.com>
2021-05-15 17:58:05 -0600

    Update tb-hotkeys.lua

--- tb-hotkeys.lua
+++ tb-hotkeys.lua
@@ -6,34 +6,9 @@
 
 -- not very hard to make at all..
 
-warn('Tower Defense Hotkeys')
-warn('Made By Vapin\' Cat#5497')
+--// Settings
 
-local connections = {}
-
-function execute()
-	if getgenv()['Tower Defense Hotkeys'] then
-		getgenv()['Tower Defense Hotkeys'].reset()
-	end
-
-	function bind(btn, key)
-		table.insert(connections,game:GetService("UserInputService").InputBegan:connect(function(inputObject, gameProcessedEvent)
-			if not gameProcessedEvent then
-				if inputObject.KeyCode == key then
-					for _,v in pairs(getconnections(btn.MouseButton1Down)) do
-						v.Function()
-					end
-					for _,v in pairs(getconnections(btn.MouseButton1Click)) do
-						v.Function()
-					end
-					for _,v in pairs(getconnections(btn.MouseButton1Up)) do
-						v.Function()
-					end
-				end
-			end
-		end))
-	end
-
+function initialize(bind) --// This is where you get a button and bind a hotkey.
 	bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.Towers.Tower1.image, Enum.KeyCode.One)
 	bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.Towers.Tower2.image, Enum.KeyCode.Two)
 	bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.Towers.Tower3.image, Enum.KeyCode.Three)
@@ -43,14 +18,47 @@ function execute()
 	bind(game:GetService("Players").LocalPlayer.PlayerGui.Stats.TowerStats.Path1.Purchase, Enum.KeyCode.E)
 end
 
-execute()
+if game.PlaceId ~= 45146873 then return end --// Game check
 
-game.Players.LocalPlayer.CharacterAdded:Connect(function()
-	game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild('Stats')
-	execute()
+warn('Tower Defense Hotkeys')
+warn('Made By Vapin\' Cat#5497')
+
+local connections = {} --// Create array for storage of connections
+
+function bind(btn, key) --// Bind function
+	table.insert(connections,game:GetService("UserInputService").InputBegan:connect(function(inputObject, gameProcessedEvent)
+		if not gameProcessedEvent then
+			if inputObject.KeyCode == key then
+				for _,v in pairs(getconnections(btn.MouseButton1Down)) do
+					v.Function()
+				end
+				for _,v in pairs(getconnections(btn.MouseButton1Click)) do
+					v.Function()
+				end
+				for _,v in pairs(getconnections(btn.MouseButton1Up)) do
+					v.Function()
+				end
+			end
+		end
+	end))
+end
+
+function execute() --// Function that contains code that runs each time you reset.
+	if getgenv()['Tower Defense Hotkeys'] then
+		getgenv()['Tower Defense Hotkeys'].reset()
+	end
+
+	initialize(bind)
+end
+
+execute() --// Run code once so you don't have to reset
+
+game.Players.LocalPlayer.CharacterAdded:Connect(function() --// Connect to the reset rbxscriptsignal
+	game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild('Stats') --// Wait for the GUIs to load in
+	execute() --// Run code after reset
 end)
 
-getgenv()['Tower Defense Hotkeys'] = {
+getgenv()['Tower Defense Hotkeys'] = { --// allows prevention of double-execution
 	['reset'] = function()
 		for _,v in pairs(connections) do
 			v:Disconnect()
